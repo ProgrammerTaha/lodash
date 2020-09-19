2af191a539333a26da7cf03a12f7489c3ef9fa48
programmertaha <programmer.taha.1y@gmail.com>
2020-09-19 14:46:46 +0300

    #1#2#3 Tasks

--- _.js
+++ _.js
@@ -10,8 +10,17 @@ todo In implementing each method, we will follow these four steps:
 
 */
 
-//create an object to contain methods
+//create an object to contain methods and then run tests on them through node _.js and node test/lodash.js
 const _ = {};
+
+//.clamp() takes three arguments: a number, a lower bound, and an upper bound
+//.clamp() modifies the provided number to be within the two provided bounds
+//If the provided number is smaller than the lower bound, it will return the lower bound as the final number
+//If the number is larger than the upper bound, it will return the upper bound as the final number
+//If the number is already within the two bounds, it will return the number as the final number
+
+
+
 
 
 
