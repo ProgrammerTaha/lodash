68b309205e904c451692d0626e04186878870f57
programmertaha <programmer.taha.1y@gmail.com>
2020-09-19 06:55:27 +0300

    #1-#2 Task

---
+++ _.js
@@ -0,0 +1,20 @@
+/*
+
+! implementing ten methods of lodash library that add new functionality for numbers, strings, objects, and arrays.
+
+todo In implementing each method, we will follow these four steps:
+% Specify the functionality of the method we are implementing
+% Ideate a game plan for how to implement this functionality in code
+% Implement our game plan
+% Test our code to ensure it works as expected
+
+*/
+
+//create an object to contain methods
+const _ = {};
+
+
+
+
+// Do not write or modify code below this line.
+module.exports = _;
